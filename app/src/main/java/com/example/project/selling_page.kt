4960de21ed7a33ee5837e6bccd5027666ae8556c
you04111 <you041111@naver.com>
2023-12-01 14:19:54 +0900

    한글로 통일, 버튼 위치 통일 등

--- app/src/main/java/com/example/project/selling_page.kt
+++ app/src/main/java/com/example/project/selling_page.kt
@@ -40,18 +40,18 @@ class selling_page : Fragment() {
         val status = arguments?.getBoolean("status", false)
 
 
-        view.findViewById<TextView>(R.id.sellingSeller).text = "판매자 : " + seller
-        view.findViewById<TextView>(R.id.sellingTitle).text = "글 제목 : " + title
-        view.findViewById<TextView>(R.id.sellingExplaination).text = "판매 설명 : " + explanation
-        view.findViewById<TextView>(R.id.sellingItemName).text = "판매 물품 : " + sellingItem
-        view.findViewById<TextView>(R.id.sellingPrice).text = "판매 가격 : " + price.toString()
+        view.findViewById<TextView>(R.id.sellingSeller).text = seller
+        view.findViewById<TextView>(R.id.sellingTitle).text = title
+        view.findViewById<TextView>(R.id.sellingExplaination).text = explanation
+        view.findViewById<TextView>(R.id.sellingItemName).text = sellingItem
+        view.findViewById<TextView>(R.id.sellingPrice).text = price.toString()+"원"
 
 
         if(status == true){
-            view.findViewById<TextView>(R.id.sellingPageStatus).text = "판매 상태 : 판매 중"
+            view.findViewById<TextView>(R.id.sellingPageStatus).text = "판매 중"
         }
         else{
-            view.findViewById<TextView>(R.id.sellingPageStatus).text = "판매 상태 : 판매 완료"
+            view.findViewById<TextView>(R.id.sellingPageStatus).text = "판매 완료"
         }
 
         println("${title}-${explanation}-${sellingItem}-${price}-${status}#######################")
